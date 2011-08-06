9f767267f700f38cbfcdac102f63d188f9d78065
Martin Blackburn <walking_fishy@hotmail.com>
2011-08-06 14:16:58 +0100

    updated to user header and footer added blank header and footer

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,8 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $this->view->header = $this->view->render('header.phtml');
+        $this->view->footer = $this->view->render('footer.phtml');
     }
 
     public function indexAction()
