8ae307f06abbec24b6155f6a441d7b01a7811212
Tom <tom@codebolt.co.uk>
2011-08-06 13:04:18 +0100

    Adding empty model and test for users

--- tests/application/models/UserModelTest.php
+++ tests/application/models/UserModelTest.php
@@ -1,14 +1,15 @@
 <?php
 
-class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+class UserModelTest extends PHPUnit_Framework_TestCase
 {
-
     public function setUp()
     {
         $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
         parent::setUp();
     }
-
-	
+    
+    public function testStart()
+    {
+    	$user = new Application_Model_User();
+    }
 }
-
