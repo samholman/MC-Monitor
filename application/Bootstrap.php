0b8d87ddfc86f0986f801c553b881fa56d4c60fc
Tom <tom@codebolt.co.uk>
2011-08-06 15:33:17 +0100

    Start of cli bits (needs alot more work)

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,19 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+	protected function _initRouter ()
+	{
+	    if (PHP_SAPI == 'cli')
+	    {
+	    	//Small hack to include the router (bored of looking for how the autoloader works in zend)
+	    	include ('application/routers/Cli.php');
+	    	
+	        $this->bootstrap ('frontcontroller');
+	        $front = $this->getResource('frontcontroller');
+	        $front->setRouter (new Application_Router_Cli ());
+	        $front->setRequest (new Zend_Controller_Request_Simple ());
+	    }
+	}
 
 }
 
