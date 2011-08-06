018ee5772ba53821b8e9127d808e483833fc7ce2
Tom <tom@codebolt.co.uk>
2011-08-07 00:46:18 +0100

    Adding server properties

--- application/models/Server.php
+++ application/models/Server.php
@@ -198,4 +198,32 @@ class Application_Model_Server
 		
 		return $playersArray;
 	}
+	
+	/**
+	 * Fetches the server properties
+	 * 
+	 * @return array
+	 */
+	public function getProperties()
+	{
+		$path = Zend_Registry::get('config')->get('minecraft')->get('worldPath') . 'server.properties';
+		
+		if (!file_exisits($path)) {
+			throw new Exception('Server properties file not found');
+		}
+		
+		$fileData = file_get_contents($path);
+		
+		$data = explode("\n", $fileData);
+		
+		$output = array();
+		
+		foreach ($data as $property)
+		{
+			list($key, $value) = explode('=', $property, 2);
+			$output[$key] = $value;
+		}
+		
+		return $output;
+	}
 }
