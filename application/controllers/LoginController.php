a5b82f361d930c2ea6bddf905d0424dee3a08308
Tom <tom@codebolt.co.uk>
2011-08-06 14:26:44 +0100

    Working on login form

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -5,12 +5,33 @@ class LoginController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        
     }
 
     public function indexAction()
     {
-        // action body
+        $form = new Application_Form_Login();
+        
+        if ($this->getRequest()->isPost())
+        {
+	        if ($form->isValid($_POST))
+	        {
+	        	$user = new Application_Model_User();
+	        	
+	        	if (!$user->authenticate($form->getElement('username')->getValue(), $form->getElement('password')->getValue()))
+	        	{
+	        		$form->addError('Authentication failed');
+	        	}
+	        	else 
+	        	{
+	        		echo 'Authenticated as: ' . $user->getUsername();
+	        	}
+	        }
+        }
+        
+        $this->view->form = $form;
+	   	$form->render($this->view);
+        
     }
 
 
