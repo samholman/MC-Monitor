6f5647f33676a863ff4e4f871449b2a6d6adb40b
Martin Blackburn <walking_fishy@hotmail.com>
2011-08-07 03:04:23 +0100

    stopped map being dragged too far right and down

--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,35 @@ var Map = Class.create(
 {
 	initialize: function()
 	{
-		this.container = $('map');
+		this.mapContainer = $('map');
 		
-		new Draggable('map');
+		this.dimensions = this.mapContainer.up(".mapBox").getDimensions();
+		
+		new Draggable('map', {onEnd: this.catchDrop.bind(this)});
 	},
+	
+	catchDrop: function(event)
+	{
+		this.dimensions = this.mapContainer.up(".mapBox").getDimensions();
+		
+		var mapPos = $('map').positionedOffset();
+		
+		//if map goes too far right, bring it back a bit
+		if(mapPos[0] > this.dimensions.width)
+		{
+			this.mapContainer.setStyle({
+				'left': '0px'
+			});
+		}
+		
+		//if map goes too far down, bring it back a bit
+		if(mapPos[1] > this.dimensions.height)
+		{
+			this.mapContainer.setStyle({
+				'top': '0px'
+			});
+		}
+	}
 });
 
 document.observe('dom:loaded', function() {
